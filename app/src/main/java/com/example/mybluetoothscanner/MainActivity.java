a66176804ce4df684bcc83d38d8c5740ff925547
jhhan00 <48585234+jhhan00@users.noreply.github.com>
2019-04-12 00:32:34 +0900

    second commit

--- app/src/main/java/com/example/mybluetoothscanner/MainActivity.java
+++ app/src/main/java/com/example/mybluetoothscanner/MainActivity.java
@@ -33,9 +33,7 @@ public class MainActivity extends AppCompatActivity {
     final static int BLUETOOTH_REQUEST_CODE = 100;
 
     //UI
-    TextView txtState;
     Button btnSearch;
-    CheckBox chkFindme;
     ListView listPaired;
     ListView listDevice;
 
@@ -47,37 +45,31 @@ public class MainActivity extends AppCompatActivity {
     List<Map<String,String>> dataDevice;
     List<Map<String,String>> dataPaired;
 
-    //permission 관련 변수
-    private final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 1;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //UI
-        txtState = (TextView)findViewById(R.id.txtState);
-        chkFindme = (CheckBox)findViewById(R.id.chkFindme);
         btnSearch = (Button)findViewById(R.id.btnSearch);
         listDevice = (ListView)findViewById(R.id.listDevice);
         listPaired = (ListView)findViewById(R.id.listPaired);
 
         //퍼미션 강제 발생코드
         ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
 
         //Adapter1
         dataPaired = new ArrayList<>();
         adapterPaired = new SimpleAdapter(this, dataPaired,
-                android.R.layout.simple_list_item_2, new String[]{"name","address"},
+                android.R.layout.simple_list_item_2, new String[]{"name","uuid"},
                 new int[]{android.R.id.text1, android.R.id.text2});
         listPaired.setAdapter(adapterPaired);
 
         //Adapter2
         dataDevice = new ArrayList<>();
         adapterDevice = new SimpleAdapter(this, dataDevice,
-                android.R.layout.simple_list_item_2, new String[]{"name","address"},
+                android.R.layout.simple_list_item_2, new String[]{"name","uuid"},
                 new int[]{android.R.id.text1, android.R.id.text2});
         listDevice.setAdapter(adapterDevice);
 
@@ -92,20 +84,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         //블루투스 브로드캐스트 리시버 등록
-        //리시버1
-        IntentFilter stateFilter = new IntentFilter();
-        stateFilter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED); //BluetoothAdapter.ACTION_STATE_CHANGED : 블루투스 상태변화 액션
-        registerReceiver(mBluetoothStateReceiver, stateFilter);
-        //리시버2
         IntentFilter searchFilter = new IntentFilter();
         searchFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED); //BluetoothAdapter.ACTION_DISCOVERY_STARTED : 블루투스 검색 시작
         searchFilter.addAction(BluetoothDevice.ACTION_FOUND); //BluetoothDevice.ACTION_FOUND : 블루투스 디바이스 찾음
         searchFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED); //BluetoothAdapter.ACTION_DISCOVERY_FINISHED : 블루투스 검색 종료
         registerReceiver(mBluetoothSearchReceiver, searchFilter);
-        //리시버3
-        IntentFilter scanmodeFilter = new IntentFilter();
-        scanmodeFilter.addAction(BluetoothAdapter.ACTION_SCAN_MODE_CHANGED);
-        registerReceiver(mBluetoothScanmodeReceiver, scanmodeFilter);
 
         //블루투스가 꺼져있으면 사용자에게 활성화 요청하기
         if(!mBluetoothAdapter.isEnabled()){
@@ -115,32 +98,6 @@ public class MainActivity extends AppCompatActivity {
             GetListPairedDevice();
         }
     }
-
-    //블루투스 상태변화 BroadcastReceiver
-    BroadcastReceiver mBluetoothStateReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            //BluetoothAdapter.EXTRA_STATE : 블루투스의 현재상태 변화
-            int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, -1);
-
-            //블루투스 활성화
-            if(state == BluetoothAdapter.STATE_ON){
-                txtState.setText("블루투스 활성화");
-            }
-            //블루투스 활성화 중
-            else if(state == BluetoothAdapter.STATE_TURNING_ON){
-                txtState.setText("블루투스 활성화 중...");
-            }
-            //블루투스 비활성화
-            else if(state == BluetoothAdapter.STATE_OFF){
-                txtState.setText("블루투스 비활성화");
-            }
-            //블루투스 비활성화 중
-            else if(state == BluetoothAdapter.STATE_TURNING_OFF){
-                txtState.setText("블루투스 비활성화 중...");
-            }
-        }
-    };
 
     //블루투스 검색결과 BroadcastReceiver
     BroadcastReceiver mBluetoothSearchReceiver = new BroadcastReceiver() {
@@ -160,7 +117,7 @@ public class MainActivity extends AppCompatActivity {
                     //데이터 저장
                     Map map = new HashMap();
                     map.put("name", device.getName()); //device.getName() : 블루투스 디바이스의 이름
-                    map.put("address", device.getAddress()); //device.getAddress() : 블루투스 디바이스의 MAC 주소
+                    map.put("uuid", device.getAddress()); //device.getAddress() : 블루투스 디바이스의 MAC 주소
                     dataDevice.add(map);
                     //리스트 목록갱신
                     adapterDevice.notifyDataSetChanged();
@@ -177,31 +134,11 @@ public class MainActivity extends AppCompatActivity {
                         //데이터 저장
                         Map map2 = new HashMap();
                         map2.put("name", paired.getName()); //device.getName() : 블루투스 디바이스의 이름
-                        map2.put("address", paired.getAddress()); //device.getAddress() : 블루투스 디바이스의 MAC 주소
+                        map2.put("uuid", paired.getUuids()); //device.getUuids() : 블루투스 디바이스의 UUID
                         dataPaired.add(map2);
                         //리스트 목록갱신
                         adapterPaired.notifyDataSetChanged();
                     }
-                    break;
-            }
-        }
-    };
-
-    //블루투스 검색응답 모드 BroadcastReceiver
-    BroadcastReceiver mBluetoothScanmodeReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            int state = intent.getIntExtra(BluetoothAdapter.EXTRA_SCAN_MODE, -1);
-            switch (state){
-                case BluetoothAdapter.SCAN_MODE_CONNECTABLE:
-                case BluetoothAdapter.SCAN_MODE_NONE:
-                    chkFindme.setChecked(false);
-                    chkFindme.setEnabled(true);
-                    Toast.makeText(MainActivity.this, "검색응답 모드 종료", Toast.LENGTH_SHORT).show();
-                    break;
-                case BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE:
-                    Toast.makeText(MainActivity.this, "다른 블루투스 기기에서 내 휴대폰을 찾을 수 있습니다.",
-                            Toast.LENGTH_SHORT).show();
                     break;
             }
         }
@@ -220,23 +157,6 @@ public class MainActivity extends AppCompatActivity {
         mBluetoothAdapter.startDiscovery();
     }
 
-    //검색응답 모드 - 블루투스가 외부 블루투스의 요청에 답변하는 슬레이브 상태
-    //BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE : 검색응답 모드 활성화 + 페이지 모드 활성화
-    //BluetoothAdapter.SCAN_MODE_CONNECTABLE : 검색응답 모드 비활성화 + 페이지 모드 활성화
-    //BluetoothAdapter.SCAN_MODE_NONE : 검색응답 모드 비활성화 + 페이지 모드 비활성화
-    //검색응답 체크박스 클릭
-    public void mOnChkFindme(View v){
-        //검색응답 체크
-        if(chkFindme.isChecked()){
-            if(mBluetoothAdapter.getScanMode() != BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE){
-                //검색응답 모드가 활성화이면 하지 않음
-                Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-                intent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 60);  //60초 동안 상대방이 나를 검색할 수 있도록한다
-                startActivity(intent);
-            }
-        }
-    }
-
     //이미 페어링된 목록 가져오기
     public void GetListPairedDevice(){
         Set<BluetoothDevice> pairedDevice = mBluetoothAdapter.getBondedDevices();
@@ -247,7 +167,7 @@ public class MainActivity extends AppCompatActivity {
                 //데이터 저장
                 Map map = new HashMap();
                 map.put("name", device.getName()); //device.getName() : 블루투스 디바이스의 이름
-                map.put("address", device.getAddress()); //device.getAddress() : 블루투스 디바이스의 MAC 주소
+                map.put("uuid", device.getUuids()); //device.getUuids() : 블루투스 디바이스의 UUID
                 dataPaired.add(map);
             }
         }
@@ -275,7 +195,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onDestroy() {
-        unregisterReceiver(mBluetoothStateReceiver);
         unregisterReceiver(mBluetoothSearchReceiver);
         super.onDestroy();
     }
